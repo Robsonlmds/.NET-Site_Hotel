4dc0779e0c74d058c15e4d62b7124856837bca2a
RobsonLMDS <98341530+Robsonlmds@users.noreply.github.com>
2024-10-18 15:06:28 -0300

    Adicionar arquivos de projeto.

---
+++ SiteHotel/Models/DataClient.cs
@@ -0,0 +1,12 @@
+﻿namespace SiteHotel.Models
+{
+    public class DataClient
+    {
+        public int QtdAdults { get; set; }
+        public int QtdChildren { get; set; }
+        public required string SuiteName { get; set; } //required 
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+        public required string Cusine { get; set; } //required 
+    } 
+}
