1c3ff023cd08e55784ec05eb8bfd887b0e840d10
RobsonLMDS <98341530+Robsonlmds@users.noreply.github.com>
2024-10-21 18:19:37 -0300

    Update - 21/10

--- SiteHotel/Controllers/DataClientController.cs
+++ SiteHotel/Controllers/DataClientController.cs
@@ -12,8 +12,11 @@ namespace SiteHotel.Controllers
         }
 
         [HttpPost]
-        public IActionResult Index(string inputData)
+        public IActionResult Index(string qtdAdults, string qtdQtdChildren, string suiteName, DateOnly checkIn, DateOnly checkOut, string bedLinen)
         {
+            string allContent = "Qtd de adultos: " + qtdAdults + " Qtd de Crianças: " + qtdQtdChildren + " Suíte: " + suiteName + " CheckIn: " + checkIn + " CheckOut " + checkOut + " Roupa de cama: " + bedLinen;
+
+            ViewBag.ShowAllContent = allContent;
             return View("HomePage");
         }
 
